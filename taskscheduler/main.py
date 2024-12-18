dca3f8e3fd104a3f6a7f65d9b409252a68b8a0ec
Michael Castil <castilmichael08@gmail.com>
2024-12-18 23:51:07 +0800

    Update version to 1.0.2

--- taskscheduler/main.py
+++ taskscheduler/main.py
@@ -2,10 +2,6 @@
 from tkinter import Tk
 from taskscheduler.ui import TaskSchedulerApp
 
-def main():
-    root = Tk()
-    app = TaskSchedulerApp(root)
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+root = Tk()
+app = TaskSchedulerApp(root)
+root.mainloop()
